fc1c7a9bffecf380b728fc03ea52027a39bb7bc3
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-11-20 10:01:10 +0700

    fix: k6 test failed

--- src/backend/app/utils/rate_limit/__init__.py
+++ src/backend/app/utils/rate_limit/__init__.py
@@ -1,8 +1,16 @@
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from flask import Flask
+from ...utils.log import log
+import os
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["50/minute"])
+if os.getenv("RATE_LIMIT_BY_X_FORWARDED_FOR", "0").lower() in ['true', '1', 'yes', 'enabled', 'on', 'enable']:
+    from flask import request
+    key_func = lambda: request.headers.get("X-Forwarded-For", request.remote_addr) or "127.0.0.1"
+else:
+    from flask_limiter.util import get_remote_address
+    key_func = get_remote_address
+
+limiter = Limiter(key_func=key_func, default_limits=["50/minute"])
 
 def setup_rate_limiter(app: Flask):
     global limiter
--- src/backend/tests/k6/load_test.js
+++ src/backend/tests/k6/load_test.js
@@ -1,3 +1,12 @@
+/**
+ * Each iteration creates 1 request and wait 1 second.
+ * In the test duration of 30s, each VU runs about 30 requests.
+ * That does not cross the rate limit (50 reqs/min)
+ * so this test should success, provided that the
+ * environment variable RATE_LIMIT_BY_X_FORWARDED_FOR
+ * is set to True.
+ */
+
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
@@ -13,7 +22,27 @@ export const options = {
 
 // --- Test logic ---
 export default function () {
-  const res = http.get('http://localhost:5000/');
+  // Generate a fake but unique IPv4 per VU
+  /**
+   * Gemini:
+   * 
+   * In k6, __VU is an execution context variable that provides
+   * information about the current Virtual User (VU).
+   * 
+   * Specifically, __VU represents the ID of the current Virtual User.
+   * 
+   * Each VU in a k6 test script runs independently, simulating
+   * a separate user interacting with your system. The __VU variable
+   * allows you to differentiate between these individual VUs during
+   * test execution.
+   */
+  const ip = `10.0.${Math.floor(__VU / 256)}.${__VU % 256}`;
+
+  const headers = {
+    'X-Forwarded-For': ip,
+  };
+
+  const res = http.get('http://localhost:5000/', { headers });
 
   check(res, {
     'status is 200': (r) => r.status === 200,
